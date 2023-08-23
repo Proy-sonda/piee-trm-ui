650e8be92d62ec3e667a90d282c837fc73edf931
Alonso Ruiz <alonso.ruiz@sonda.com>
2023-08-23 11:41:19 -0400

    Mover modal de editar usuario a nuevo componente

--- app/empleadores/usuariosrrhh/page.tsx
+++ app/empleadores/usuariosrrhh/page.tsx
@@ -3,6 +3,7 @@
 import Position from '@/app/components/stage/Position';
 import Link from 'next/link';
 import React from 'react';
+import ModalModificarUsuario from './(componentes)/ModalModificarUsuario';
 import TablaUsuarios from './(componentes)/TablaUsuarios';
 
 interface UsuariosRRHHPageProps {}
@@ -98,6 +99,7 @@ const UsuariosRRHHPage: React.FC<UsuariosRRHHPageProps> = ({}) => {
           </div>
           <div className="row mt-3">
             <div className="text-end">
+              {/* TODO: Revisar bien como volver cuando no se usen query params */}
               <Link
                 href={'/empleadores/unidad?rut=76279970-7&razon=BONILLA%20Y%20GOMEZ%200LTDA.'}
                 className="btn btn-danger">
@@ -105,69 +107,8 @@ const UsuariosRRHHPage: React.FC<UsuariosRRHHPageProps> = ({}) => {
               </Link>
             </div>
           </div>
-          <br />
-        </div>
 
-        <div
-          className="modal fade"
-          id="modusr"
-          tabIndex={-1}
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true">
-          <div className="modal-dialog modal-lg">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">
-                  Modificar Usuario
-                </h1>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <div className="row">
-                  <div className="col-md-6">
-                    <label>RUN</label>
-                    <input type="text" className="form-control" disabled value={'111111'} />
-                  </div>
-                  <div className="col-md-6">
-                    <label>Nombre</label>
-                    <input type="text" className="form-control" value={'Juan'} />
-                  </div>
-                </div>
-                <div className="row mt-2">
-                  <div className="col-md-6">
-                    <label>Apellido</label>
-                    <input type="text" className="form-control" value={'Rodriguez'} />
-                  </div>
-                  <div className="col-md-6">
-                    <label>Correo</label>
-                    <input type="text" className="form-control" value={'juan@gmail.com'} />
-                  </div>
-                </div>
-                <div className="row mt-2">
-                  <div className="col-md-6">
-                    <label>RRHH</label>
-                    <select className="form-select">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-primary">
-                  Modificar
-                </button>
-                <button type="button" className="btn btn-success" data-bs-dismiss="modal">
-                  Volver
-                </button>
-              </div>
-            </div>
-          </div>
+          <ModalModificarUsuario />
         </div>
       </div>
     </>
