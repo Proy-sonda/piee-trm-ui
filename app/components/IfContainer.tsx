162ede6beea775afe1170cb45594ec4b0f9b54c2
Alonso Ruiz <alonso.ruiz@sonda.com>
2023-08-28 14:27:54 -0400

    Documentar el if container

--- app/components/IfContainer.tsx
+++ app/components/IfContainer.tsx
@@ -5,6 +5,33 @@ interface IfContainerProps {
   children: ReactNode | (() => ReactNode);
 }
 
+/**
+ * Muestra el contenido condicionalmente. Se pueden pasar los children directamente de esta forma
+ *
+ * ```jsx
+ *  <IfContainer show={algunaCondicion}>
+ *    <div>{algo}</div>
+ *    {// etc }
+ *  </IfContainer>
+ * ```
+ *
+ * En este caso se hace la evaluación de los children independiente de si la condicion es verdadera
+ * o falsa.
+ *
+ * En caso de que hayan propiedades que son `null` o `undefined` dentro de los children
+ * se puede pasar un callback con los children a renderizar de esta forma
+ *
+ * ```jsx
+ *  <IfContainer show={algunaCondicion}>
+ *    {() => (
+ *      <div>{algunaVariablePosiblementeUndefined}</div>
+ *    )}
+ *  </IfContainer>
+ * ```
+ *
+ * Esto renderizara los children solo cuando se cumpla la condicion y no antes, lo que permitira
+ * retrasar la evaluacion de las variables hasta el ultimo momento.
+ */
 const IfContainer: React.FC<IfContainerProps> = ({ show, children }) => {
   if (!show) {
     return null;
