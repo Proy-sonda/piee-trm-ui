a7284849700b0cee09c61bd2d8251e7af6ac699b
Alonso Ruiz <alonso.ruiz@sonda.com>
2023-09-05 15:01:38 -0300

    Corregir error de tipado

--- src/app/empleadores/(servicios)/buscar-actividades-laborales.ts
+++ src/app/empleadores/(servicios)/buscar-actividades-laborales.ts
@@ -3,6 +3,6 @@ import { ActividadLaboral } from '../(modelos)/actividad-laboral';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export const buscarActividadesLaborales = (): [() => Promise<any>, () => void] => {
+export const buscarActividadesLaborales = () => {
   return runFetchAbortable<ActividadLaboral[]>(`${apiUrl}actividadlaboral/all`);
 };
