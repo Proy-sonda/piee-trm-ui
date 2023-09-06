fce72cd158932d7c32ef247db54fb66b7e753099
Alonso Ruiz <alonso.ruiz@sonda.com>
2023-09-06 18:58:45 -0300

    Corregir errores de compilacion

--- src/app/empleadores/unidad/usuarios/(servicios)/buscar-empleador-rut.ts
+++ src/app/empleadores/unidad/usuarios/(servicios)/buscar-empleador-rut.ts
@@ -1,20 +1,18 @@
 import { obtenerToken } from '@/servicios/auth';
-import { runFetch } from '@/servicios/fetch';
+import { runFetchAbortable } from '@/servicios/fetch';
 import { Empleadorusu } from '../(modelos)/iempleadorusu';
 
 const api_url = process.env.NEXT_PUBLIC_API_URL;
 
 export const buscarEmpleadorRut = (rut: string) => {
-  return runFetch<Empleadorusu>(
-    fetch(`${api_url}empleador/rutempleador`, {
-      method: 'POST',
-      headers: {
-        Authorization: obtenerToken(),
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        rutempleador: rut,
-      }),
+  return runFetchAbortable<Empleadorusu>(`${api_url}empleador/rutempleador`, {
+    method: 'POST',
+    headers: {
+      Authorization: obtenerToken(),
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      rutempleador: rut,
     }),
-  );
+  });
 };
