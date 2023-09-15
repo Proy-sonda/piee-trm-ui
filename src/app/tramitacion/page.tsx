7bdcb2e87bf70d79292504455f4a5a38a56094db
aruizaguila <alonso.ruiz@sonda.com>
2023-09-15 10:41:35 -0300

    Corregir problema que no deja cerrar sesion en tramitacion

--- src/app/tramitacion/page.tsx
+++ src/app/tramitacion/page.tsx
@@ -2,14 +2,11 @@
 
 import Position from '@/components/stage/position';
 import { AuthContext } from '@/contexts';
-import { ComboEntidadEmpleador } from '@/helpers/tramitacion/empleadores';
 import { useForm } from '@/hooks/use-form';
 import { Empleador } from '@/modelos/empleador';
 import { Unidadrhh } from '@/modelos/tramitacion';
 import { cargaUnidadrrhh } from '@/servicios/carga-unidad-rrhh';
-import jwt_decode from 'jwt-decode';
-import { parseCookies } from 'nookies';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { Table, Tbody, Th, Thead, Tr } from 'react-super-responsive-table';
 import styles from './tramitacion.module.css';
 
@@ -28,6 +25,8 @@ const TramitacionPage = () => {
     entidadempleador: '',
     urrhh: '',
   });
+
+  /* TODO: Usar los hooks para obtener la entidad empleadora y no usar cookies en esta parte
   let cookie = parseCookies();
   let token = '';
   token = cookie.token;
@@ -44,7 +43,7 @@ const TramitacionPage = () => {
     };
 
     EntidadEmpleadora();
-  }, [token]);
+  }, [token]);  */
 
   const onChangeEmp = (event: any) => {
     let value = event.target.value;
