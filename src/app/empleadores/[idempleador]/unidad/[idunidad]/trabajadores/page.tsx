1bd63874e2d0ee6433ec99c813f2e649d680c29c
Esteban Lepe <131920412+elepec29@users.noreply.github.com>
2023-09-22 17:31:17 -0300

    spinner carga trabajadores

--- src/app/empleadores/[idempleador]/unidad/[idunidad]/trabajadores/page.tsx
+++ src/app/empleadores/[idempleador]/unidad/[idunidad]/trabajadores/page.tsx
@@ -26,6 +26,7 @@ import { formatRut, validateRut } from 'rutlib';
 import Swal from 'sweetalert2';
 
 import { buscarEmpleadorPorId } from '@/app/empleadores/[idempleador]/datos/(servicios)/buscar-empleador-por-id';
+import SpinnerPantallaCompleta from '@/components/spinner-pantalla-completa';
 import styles from './trabajadores.module.css';
 
 interface TrabajadoresPageProps {
@@ -38,6 +39,7 @@ interface TrabajadoresPageProps {
 const TrabajadoresPage: React.FC<TrabajadoresPageProps> = ({ params }) => {
   const [unidad, setunidad] = useState('');
   const [arrerror, setarrerror] = useState(false);
+  const [spinnerCargar, setspinnerCargar] = useState(false);
   const [rutconerror, setrutconerror] = useState<any[]>([]);
   const [unidadEmpleador, setunidadEmpleador] = useState<UnidadEmpleador[]>([]);
   const [razon, setRazon] = useState('');
@@ -162,7 +164,6 @@ const TrabajadoresPage: React.FC<TrabajadoresPageProps> = ({ params }) => {
     e.preventDefault();
 
     if (error.run) return;
-    setLoading(true);
 
     const crearTrabajadorAux = async () => {
       const data = await crearTrabajador({
@@ -225,8 +226,7 @@ const TrabajadoresPage: React.FC<TrabajadoresPageProps> = ({ params }) => {
     }
 
     let cuentaGrabados = 0;
-
-    setLoading(true);
+    setspinnerCargar(true);
 
     for (let index = 0; index < csvData.length; index++) {
       const element = csvData[index];
@@ -262,18 +262,18 @@ const TrabajadoresPage: React.FC<TrabajadoresPageProps> = ({ params }) => {
         showConfirmButton: false,
         timer: 2000,
         didClose: () => {
-          setLoading(false);
+          setspinnerCargar(false);
           if (rutconerror.length > 0) setarrerror(true);
         },
       });
       refrescarComponente();
     } else {
-      setLoading(false);
       Swal.fire({
         icon: 'info',
         html: 'No se ha añadido ningún trabajador',
         confirmButtonColor: 'var(--color-blue)',
         didClose: () => {
+          setspinnerCargar(false);
           if (rutconerror.length > 0) setarrerror(true);
         },
       });
@@ -310,6 +310,9 @@ const TrabajadoresPage: React.FC<TrabajadoresPageProps> = ({ params }) => {
         </Modal.Body>
         <Modal.Footer></Modal.Footer>
       </Modal>
+      <IfContainer show={spinnerCargar}>
+        <SpinnerPantallaCompleta />
+      </IfContainer>
       <div className="bgads">
         <div className="me-5 ms-5 animate__animate animate__fadeIn">
           <div className="row mt-5">
