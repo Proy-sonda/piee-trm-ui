e19d4f4b210c9793af96ef0cf20c40754d48de6b
aruizaguila <alonso.ruiz@sonda.com>
2023-09-25 18:50:19 -0300

    Mover estilos del spinner a componente del spinner

--- src/components/spinner-pantalla-completa.tsx
+++ src/components/spinner-pantalla-completa.tsx
@@ -6,7 +6,21 @@ interface SpinnerPantallaCompletaProps {}
 const SpinnerPantallaCompleta: React.FC<SpinnerPantallaCompletaProps> = ({}) => {
   return (
     <>
-      <div className={'spinner'}>
+      <div
+        className={'spinner'}
+        style={{
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          width: '100%',
+          height: '100%',
+          backgroundColor:
+            'rgba( 223, 217, 217, 0.5)' /* Fondo semi-transparente para difuminar el contenido */,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: '9999',
+        }}>
         <ClipLoader
           color={'var(--color-blue)'}
           loading={true}
