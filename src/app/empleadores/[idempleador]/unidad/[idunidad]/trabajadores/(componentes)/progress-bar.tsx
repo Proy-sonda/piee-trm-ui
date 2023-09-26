e5ff6d0c6510f5f4fbf74eb9c7a6414d1b374f52
aruizaguila <alonso.ruiz@sonda.com>
2023-09-26 09:50:59 -0300

    Mover estilos de la progress bar del global.css a uno propio

--- src/app/empleadores/[idempleador]/unidad/[idunidad]/trabajadores/(componentes)/progress-bar.tsx
+++ src/app/empleadores/[idempleador]/unidad/[idunidad]/trabajadores/(componentes)/progress-bar.tsx
@@ -1,4 +1,5 @@
 import { ProgressBar } from 'react-bootstrap';
+import styles from './progress-bar.module.css';
 
 interface props {
   show: boolean;
@@ -10,12 +11,12 @@ export const ProgressBarCustom: React.FC<props> = ({ show, text, count }) => {
   return (
     <>
       <div
-        className="progress_bar"
+        className={styles['progress_bar']}
         style={{
           display: show ? 'block' : 'none',
         }}>
-        <div className="progres">
-          <p>{text}</p>
+        <div className={styles['progres']}>
+          <p className={styles['texto_progress_bar']}>{text}</p>
           <ProgressBar animated now={count} label={`${count.toFixed(1)}%`} />
         </div>
       </div>
