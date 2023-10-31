341441d850ab972c8da9394763b720c3b6a452eb
aruizaguila <alonso.ruiz@sonda.com>
2023-10-31 16:11:51 -0300

    Intento 1

--- src/components/form/input-mes-ano.tsx
+++ src/components/form/input-mes-ano.tsx
@@ -1,7 +1,7 @@
 import { InputReciclableBase, UnibleConFormArray } from '@/components/form';
 import { esFechaInvalida } from '@/utilidades/es-fecha-invalida';
-import { endOfDay, isAfter, isBefore, parse, startOfMonth } from 'date-fns';
-import React from 'react';
+import { endOfDay, format, isAfter, isBefore, parse, startOfMonth } from 'date-fns';
+import React, { useRef, useState } from 'react';
 import { Form, FormGroup } from 'react-bootstrap';
 import { useFormContext } from 'react-hook-form';
 import { useInputReciclable } from './hooks';
@@ -22,6 +22,44 @@ export const InputMesAno: React.FC<InputMesAnoProps> = ({
 }) => {
   const { register } = useFormContext();
 
+  const [textoFecha, setTextoFecha] = useState('');
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const { ref, ...resto } = register(name, {
+    setValueAs: (date: string) => {
+      /** Situa la fecha con respecto al inicio de hoy */
+      const date2 = parse(date, 'yyyy-MM', startOfMonth(new Date()));
+
+      if (!esFechaInvalida(date2)) {
+        setTextoFecha(format(date2, 'MMMM - yyyy'));
+      }
+
+      return date2;
+    },
+    required: {
+      value: !opcional,
+      message: 'La fecha es obligatoria',
+    },
+    validate: {
+      esFechaValida: (fecha: Date) => {
+        if (!opcional && esFechaInvalida(fecha)) {
+          return 'La fecha es inválida';
+        }
+      },
+      despuesDe1920: (fecha: Date) => {
+        if (isBefore(fecha, new Date(1920, 11, 31))) {
+          return 'Debe ser mayor o igual a enero de 1921';
+        }
+      },
+      noMayorQueHoy: (fecha: Date) => {
+        if (isAfter(fecha, endOfDay(Date.now()))) {
+          return 'No puede ser posterior a hoy';
+        }
+      },
+    },
+  });
+
   const { idInput, textoLabel, tieneError, mensajeError } = useInputReciclable({
     prefijoId: 'mesano',
     name,
@@ -35,36 +73,26 @@ export const InputMesAno: React.FC<InputMesAnoProps> = ({
         {textoLabel && <Form.Label>{textoLabel}</Form.Label>}
 
         <Form.Control
+          type="text"
+          readOnly
+          value={textoFecha}
+          onClick={() => {
+            if (inputRef.current) {
+              inputRef.current.showPicker();
+            }
+          }}
+        />
+
+        <Form.Control
           type="month"
+          style={{ display: 'none' }}
           autoComplete="new-custom-value"
           isInvalid={tieneError}
-          {...register(name, {
-            setValueAs: (date: string) => {
-              /** Situa la fecha con respecto al inicio de hoy */
-              return parse(date, 'yyyy-MM', startOfMonth(new Date()));
-            },
-            required: {
-              value: !opcional,
-              message: 'La fecha es obligatoria',
-            },
-            validate: {
-              esFechaValida: (fecha: Date) => {
-                if (!opcional && esFechaInvalida(fecha)) {
-                  return 'La fecha es inválida';
-                }
-              },
-              despuesDe1920: (fecha: Date) => {
-                if (isBefore(fecha, new Date(1920, 11, 31))) {
-                  return 'Debe ser mayor o igual a enero de 1921';
-                }
-              },
-              noMayorQueHoy: (fecha: Date) => {
-                if (isAfter(fecha, endOfDay(Date.now()))) {
-                  return 'No puede ser posterior a hoy';
-                }
-              },
-            },
-          })}
+          ref={(e: any) => {
+            ref(e);
+            inputRef.current = e;
+          }}
+          {...resto}
         />
 
         <Form.Control.Feedback type="invalid" tooltip>
