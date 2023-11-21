f9ace20ccdf61142e8bcdad5c3ba7890bd8a8160
aruizaguila <alonso.ruiz@sonda.com>
2023-11-21 17:25:31 -0300

    Mandar solo la data en base 64, no como Data URL

--- src/utilidades/file-to-base64.ts
+++ src/utilidades/file-to-base64.ts
@@ -1,19 +1,21 @@
 /**
- * Convierte un archivo a un string en base 64 con formato
- * [Data URL](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs).
- *
- * @returns
- * Un string de esta forma: `data:application/pdf;base64,JVBERi0xLjcNCiW1tbW1...`
+ * Convierte un archivo a un string en base 64.
  */
 export const fileToBase64 = (archivo: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
 
     reader.onload = () => {
-      if (typeof reader.result === 'string') {
-        resolve(reader.result);
-      } else {
+      if (typeof reader.result !== 'string') {
         reject(new Error('No se pudo convertir el archivo a base 64'));
+        return;
+      }
+
+      const datosBase64 = reader.result.split(',').pop();
+      if (!datosBase64) {
+        reject(new Error(`No puede obtener datos de archivo "${archivo.name}"`));
+      } else {
+        resolve(datosBase64);
       }
     };
 
