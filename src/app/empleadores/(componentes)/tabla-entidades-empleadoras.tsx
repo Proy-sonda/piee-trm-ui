4c32d97bb044e44ff461607a28a9666351ae15cf
Esteban Lepe <131920412+elepec29@users.noreply.github.com>
2023-12-07 16:49:54 -0300

    modificación texto alerta

--- src/app/empleadores/(componentes)/tabla-entidades-empleadoras.tsx
+++ src/app/empleadores/(componentes)/tabla-entidades-empleadoras.tsx
@@ -41,7 +41,7 @@ export default function TablaEntidadesEmpleadoras({
       html: `<b>1.- </b> La desadscripción del portal PIEE implica que la entidad empleadora no podrá tramitar Licencias médicas de forma electrónica,
       debiendo realizar este trámite de forma manual en el asegurador correspondiente a la persona trabajadora. <br/><br/>
       <b>2.- </b> La persona usuaria al realizar la solicitud de desadscripción declara conocer y aceptar los cambios que se generan en la tramitación de las Licencias Médicas indicadas en el punto anterior. <br/><br/>
-      <b>3.- </b> Al realizar esta solicitud, la persona administradora podrá aceptar o rechazar la desadscripción de la entidad empleadora.`,
+      <b>3.- </b> Al realizar esta solicitud, la persona administradora del portal podrá aceptar o rechazar esta solicitud, por lo que la entidad empleadora continuara activa hasta que se resuelva la solicitud. <br/><br/>`,
       confirmButtonText: 'Aceptar',
       confirmButtonColor: 'var(--color-blue)',
       denyButtonColor: 'var(--bs-danger)',
