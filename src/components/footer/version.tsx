a48094289291593bd03b56cc232ebdf593b8d488
aruizaguila <alonso.ruiz@sonda.com>
2023-12-14 13:38:07 -0300

    Fix: Agregar cambios pendientes en version de la app
    commit 1074e962205ed9e152992c7760bb624ac063f569
    Author: aruizaguila <alonso.ruiz@sonda.com>
    Date:   Thu Dec 14 13:32:48 2023 -0300
    
        Actualizar version por defecto
    
    commit 32c0465b6107818a4e6056f6a43bf06f08cdf5f6
    Author: aruizaguila <alonso.ruiz@sonda.com>
    Date:   Thu Dec 14 11:59:18 2023 -0300
    
        Agregar variable de version al dockerfile
    
    commit 6d8d1d04442e7b51cee6dcc530f8d1eb01a46d85
    Author: aruizaguila <alonso.ruiz@sonda.com>
    Date:   Thu Dec 14 11:04:11 2023 -0300
    
        Agregar versionamiento semantico
    
    commit 88458fe23b5a95cf65c0c46c9a8084e62b795a67
    Author: aruizaguila <alonso.ruiz@sonda.com>
    Date:   Thu Dec 14 10:39:46 2023 -0300
    
        Instalar git en el contenedor
    
    commit 8305264eae7d9136e4cf0cf5ea9ba74a6c989de5
    Author: aruizaguila <alonso.ruiz@sonda.com>
    Date:   Thu Dec 14 10:36:10 2023 -0300
    
        Colocar bien el formato de la fecha en la version
    
    commit f6031817e0316af4df300dd08716e52b9b965624
    Author: aruizaguila <alonso.ruiz@sonda.com>
    Date:   Wed Dec 13 19:23:11 2023 -0300
    
        Crear script para generar la version

--- src/components/footer/version.tsx
+++ src/components/footer/version.tsx
@@ -3,13 +3,13 @@ import { readFile } from 'fs/promises';
 import path from 'path';
 
 export default async function Version() {
-  let version = 'DEV';
+  let version = 'DESARROLLO';
   const rutaArchivoInformacionApp = path.join(process.cwd(), 'public', 'appinfo.json');
 
   if (existsSync(rutaArchivoInformacionApp)) {
     const appInfoJSON = await readFile(rutaArchivoInformacionApp, 'utf-8');
     const appInfo = JSON.parse(appInfoJSON);
-    version = appInfo.version ?? 'Desarrollo';
+    version = appInfo.version ?? 'DESARROLLO';
   }
 
   return <div className="mt-2 text-center text-secondary">Versión: {version}</div>;
