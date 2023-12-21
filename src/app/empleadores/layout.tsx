9d68d82a514af066740889e27e0cd453b67d8b0a
Esteban Lepe <131920412+elepec29@users.noreply.github.com>
2023-12-21 18:47:44 -0300

    meta layout empleador

--- src/app/empleadores/layout.tsx
+++ src/app/empleadores/layout.tsx
@@ -3,11 +3,11 @@ import { AuthContext } from '@/contexts';
 import { useContext, useEffect, useState } from 'react';
 import { EmpleadorActualProvider } from './(contexts)/empleador-actual-context';
 
-export const metadata = {
-  title: 'Datos Entidades Empleadoras',
-  description:
-    'Acceso a una lista que detalla todos los empleadores que Usuario Administrador supervisa para la tramitación de licencias médicas. Para cada empleador, podrá gestionar los datos de la entidad, explorar las unidades de Recursos Humanos afiliadas y administrar los usuarios habilitados para la gestión de estas unidades.',
-};
+// export const metadata = {
+//   title: 'Datos Entidades Empleadoras',
+//   description:
+//     'Acceso a una lista que detalla todos los empleadores que Usuario Administrador supervisa para la tramitación de licencias médicas. Para cada empleador, podrá gestionar los datos de la entidad, explorar las unidades de Recursos Humanos afiliadas y administrar los usuarios habilitados para la gestión de estas unidades.',
+// };
 
 /** EL unico proposito de este layout es colocar el `EmpleadorActualProvider` */
 export default function EmpleadoresLayout({ children }: { children: React.ReactNode }) {
