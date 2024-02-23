719059fc4ed420c181006fa0b9ba1106930d67e7
aruizaguila <alonso.ruiz@sonda.com>
2024-02-23 13:29:49 -0300

    Hacer mas armonico el spinner pantalla completa

--- src/components/spinner-pantalla-completa.tsx
+++ src/components/spinner-pantalla-completa.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ClipLoader } from 'react-spinners';
+import { FadeLoader } from 'react-spinners';
 
 interface SpinnerPantallaCompletaProps {}
 
@@ -14,20 +14,13 @@ const SpinnerPantallaCompleta: React.FC<SpinnerPantallaCompletaProps> = ({}) => 
           left: '0',
           width: '100%',
           height: '100%',
-          backgroundColor:
-            'rgba( 223, 217, 217, 0.5)' /* Fondo semi-transparente para difuminar el contenido */,
+          backgroundColor: 'rgba( 223, 217, 217, 0.5)', // Fondo semi-transparente para difuminar el contenido
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           zIndex: '9999',
         }}>
-        <ClipLoader
-          color={'var(--color-blue)'}
-          loading={true}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
+        <FadeLoader color="var(--color-blue)" height={30} margin={10} radius={4} width={6} />
       </div>
     </>
   );
