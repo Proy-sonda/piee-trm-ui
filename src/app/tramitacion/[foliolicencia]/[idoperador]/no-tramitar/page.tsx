fa146bb271071acf229767ffb51e7508453c6f27
Esteban Lepe <131920412+elepec29@users.noreply.github.com>
2024-04-05 17:28:09 -0300

    HOTFIX: No mostrar con isapre

--- src/app/tramitacion/[foliolicencia]/[idoperador]/no-tramitar/page.tsx
+++ src/app/tramitacion/[foliolicencia]/[idoperador]/no-tramitar/page.tsx
@@ -458,7 +458,10 @@ const NoRecepcionarLicenciaPage: React.FC<NoRecepcionarLicenciaPageProps> = ({
 
                 <Col xs={12} md={5} lg={4} className="mt-4 mt-md-0">
                   <IfContainer
-                    show={(licencia && esLicenciaFONASA(licencia)) || solicitadEntidadPagadora}>
+                    show={
+                      (licencia && esLicenciaFONASA(licencia)) ||
+                      (solicitadEntidadPagadora && esLicenciaFONASA(licencia!))
+                    }>
                     <ComboSimple
                       opcional={
                         !licencia || !esLicenciaFONASA(licencia) || !solicitadEntidadPagadora
