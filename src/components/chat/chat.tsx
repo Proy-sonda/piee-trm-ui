30a214e448cf8bf55db7b756daea03f54c1c1d1f
elepec29 <esteban.lepe@sonda.com>
2024-07-10 11:56:53 -0400

    Mouseover en botón de chat

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -7,16 +7,33 @@ import styles from './chat.module.css';
 
 export const ChatComponent = () => {
   const [verChat, setverChat] = useState(false);
+  const [mouseHover, setMouseHover] = useState(false);
 
   return (
     <>
-      <button className={styles['btn-chat']} onClick={() => setverChat(!verChat)}>
-        {!verChat ? (
-          <i className="animate__animated animate_fadeIn bi bi-chat-dots-fill"></i>
-        ) : (
+      {!verChat && mouseHover ? (
+        <button
+          className={`${styles['btn-chathover']}`}
+          onClick={() => setverChat(!verChat)}
+          onMouseEnter={() => setMouseHover(true)}
+          onMouseLeave={() => setMouseHover(false)}>
+          <div className="d-flex justify-content-around">
+            <div className="p-2 align-self-center">ASISTENTE VIRTUAL</div>
+            <div className="p-2">
+              <i className="animate__animated animate_fadeIn bi bi-chat-dots-fill float-rigth"></i>
+            </div>
+          </div>
+        </button>
+      ) : (
+        <button
+          className={styles['btn-chat']}
+          onClick={() => setverChat(!verChat)}
+          onMouseEnter={() => setMouseHover(true)}
+          onMouseLeave={() => setMouseHover(false)}>
           <i className="bi bi-chat-fill"></i>
-        )}
-      </button>
+        </button>
+      )}
+
       <iframe
         className="animate__animated animate__fadeIn"
         hidden={!verChat}
