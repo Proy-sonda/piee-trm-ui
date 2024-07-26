a70d01ae4c28a84ed26c7054b474a4985d8eca2f
aruizaguila <alonso.ruiz@sonda.com>
2024-07-26 18:13:13 -0400

    Agregar boton para refrescar las licencias

--- src/app/tramitacion/page.tsx
+++ src/app/tramitacion/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { Titulo } from '@/components';
-import { useMergeFetchObject } from '@/hooks';
+import { useFetch, useMergeFetchObject, useRefrescarPagina } from '@/hooks';
 import { buscarEmpleadores } from '@/servicios';
 import { useEffect, useState } from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import {
   ExportarCSV,
   FiltroLicencias,
@@ -17,10 +18,16 @@ import { Estado } from './(modelos)/estado-licencias';
 import { buscarLicenciasParaTramitar } from './(servicios)/buscar-licencias-para-tramitar';
 
 const TramitacionPage = () => {
+  const [refresh, recargarBandejaTramitacion] = useRefrescarPagina();
+
   const [erroresCarga, datosBandeja, cargando] = useMergeFetchObject({
-    licenciasParaTramitar: buscarLicenciasParaTramitar(),
     empleadores: buscarEmpleadores(''),
   });
+
+  const [errorLicencias, licenciasParaTramitar, cargandoLicencias] = useFetch(
+    buscarLicenciasParaTramitar(),
+    [refresh],
+  );
 
   const [estado, setEstado] = useState<Estado>({
     licenciasFiltradas: [],
@@ -30,14 +37,14 @@ const TramitacionPage = () => {
 
   // Actualizar listado de licencias
   useEffect(() => {
-    if (datosBandeja?.licenciasParaTramitar) {
-      setEstado((prev) => ({ ...prev, licenciasFiltradas: datosBandeja.licenciasParaTramitar }));
+    if (licenciasParaTramitar) {
+      setEstado((prev) => ({ ...prev, licenciasFiltradas: licenciasParaTramitar }));
     }
   }, [datosBandeja]);
 
   // Filtrar licencias
   useEffect(() => {
-    const licenciasParaFiltrar = datosBandeja?.licenciasParaTramitar ?? [];
+    const licenciasParaFiltrar = licenciasParaTramitar ?? [];
 
     setEstado((prev) => ({
       ...prev,
@@ -45,11 +52,11 @@ const TramitacionPage = () => {
         licenciaCumpleFiltros(prev.filtrosBusqueda, prev.filtroEstado),
       ),
     }));
-  }, [estado.filtrosBusqueda, estado.filtroEstado, datosBandeja?.licenciasParaTramitar]); 
+  }, [estado.filtrosBusqueda, estado.filtroEstado, licenciasParaTramitar]);
 
   return (
     <>
-      <IfContainer show={cargando}>
+      <IfContainer show={cargando || cargandoLicencias}>
         <SpinnerPantallaCompleta />
       </IfContainer>
 
@@ -57,7 +64,7 @@ const TramitacionPage = () => {
         <h4 className="pb-5 text-center">Error al cargar licencias para tramitar</h4>
       </IfContainer>
 
-      <IfContainer show={erroresCarga.length === 0}>
+      <IfContainer show={erroresCarga.length === 0 || errorLicencias}>
         <div className="row">
           <Titulo url="">
             <h5>Filtro para Licencias pendientes de Tramitar</h5>
@@ -80,17 +87,35 @@ const TramitacionPage = () => {
             <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
               <div className="mb-2 mb-md-0 d-flex justify-content-start align-items-center">
                 <h2 className="fs-5 m-0 p-0">BANDEJA DE TRAMITACIÓN</h2>
-                <div className="d-md-none">
+                <div className="d-md-none d-flex align-items-center">
+                  <OverlayTrigger
+                    overlay={<Tooltip>Volver a cargar bandeja de tramitación</Tooltip>}>
+                    <button
+                      className="btn btn-sm border border-0"
+                      style={{ fontSize: '20px' }}
+                      onClick={() => recargarBandejaTramitacion()}>
+                      <i className="bi bi-arrow-clockwise"></i>
+                    </button>
+                  </OverlayTrigger>
                   <ExportarCSV
-                    licenciaTramitar={datosBandeja!?.licenciasParaTramitar}
+                    licenciaTramitar={licenciasParaTramitar ?? []}
                     empleadores={datosBandeja!?.empleadores}
                   />
                 </div>
               </div>
               <div className="d-flex justify-content-end align-items-center">
                 <div className="d-none d-md-block">
+                  <OverlayTrigger
+                    overlay={<Tooltip>Volver a cargar bandeja de tramitación</Tooltip>}>
+                    <button
+                      className="btn btn-sm border border-0"
+                      style={{ fontSize: '20px' }}
+                      onClick={() => recargarBandejaTramitacion()}>
+                      <i className="bi bi-arrow-clockwise"></i>
+                    </button>
+                  </OverlayTrigger>
                   <ExportarCSV
-                    licenciaTramitar={datosBandeja!?.licenciasParaTramitar}
+                    licenciaTramitar={licenciasParaTramitar ?? []}
                     empleadores={datosBandeja!?.empleadores}
                   />
                 </div>
