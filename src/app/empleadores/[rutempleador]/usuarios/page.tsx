5d285f664992639235f6b0f1d1b96d600be055a0
aruizaguila <alonso.ruiz@sonda.com>
2024-08-14 12:59:50 -0400

    Agregar boton para descargar CSV de los usuarios de entidad empleadora

--- src/app/empleadores/[rutempleador]/usuarios/page.tsx
+++ src/app/empleadores/[rutempleador]/usuarios/page.tsx
@@ -5,10 +5,12 @@ import { GuiaUsuario } from '@/components/guia-usuario';
 import { AuthContext } from '@/contexts';
 import { emptyFetch, useFetch } from '@/hooks/use-merge-fetch';
 import { useRefrescarPagina } from '@/hooks/use-refrescar-pagina';
-import { strIncluye } from '@/utilidades';
+import { AlertaConfirmacion, strIncluye } from '@/utilidades';
+import { format } from 'date-fns';
+import exportFromJSON from 'export-from-json';
 import dynamic from 'next/dynamic';
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col, Form, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
 import { useEmpleadorActual } from '../../(contexts)/empleador-actual-context';
 import { ModalCrearUsuario, ModalEditarUsuario } from './(componentes)';
 import TablaUsuarios from './(componentes)/tabla-usuarios';
@@ -103,6 +105,33 @@ const UsuariosPage: React.FC<UsuariosPageProps> = ({}) => {
     setUsuariosFiltrados(filtrados);
   }, [usuarios, textoBusqueda]);
 
+  const exportarUsuariosCSV = async () => {
+    const { isConfirmed } = await AlertaConfirmacion.fire({
+      html: `¿Desea exportar las personas usuarias a CSV?`,
+    });
+
+    if (!isConfirmed) {
+      return;
+    }
+
+    const data = (usuarios ?? []).map((usuario) => ({
+      RUN: usuario.rutusuario,
+      Nombre: `${usuario.nombres} ${usuario.apellidopaterno} ${usuario.apellidomaterno}`,
+      'Teléfono 1': usuario.usuarioempleadorActual.telefonouno,
+      'Teléfono 2': usuario.usuarioempleadorActual.telefonodos,
+      'Correo Electrónico': usuario.usuarioempleadorActual.email,
+      Rol: usuario.usuarioempleadorActual.rol.rol,
+    }));
+
+    exportFromJSON({
+      data,
+      fileName: `personas_usuarias_${format(Date.now(), 'dd_MM_yyyy_HH_mm_ss')}`,
+      exportType: exportFromJSON.types.csv,
+      delimiter: ';',
+      withBOM: true,
+    });
+  };
+
   return (
     <>
       <Titulo url="">
@@ -120,14 +149,24 @@ const UsuariosPage: React.FC<UsuariosPageProps> = ({}) => {
           />
         </Col>
 
-        <IfContainer show={rolEnEmpleadorActual === 'administrador'}>
-          <Col
-            xs={12}
-            sm={4}
-            md={6}
-            lg={7}
-            xxl={8}
-            className="d-flex flex-column flex-md-row justify-content-sm-end">
+        <Col
+          xs={12}
+          sm={4}
+          md={6}
+          lg={7}
+          xxl={8}
+          className="d-flex flex-row flex-md-row justify-content-sm-end">
+          <div>
+            <OverlayTrigger overlay={<Tooltip>Exportar personas usuarias a CSV</Tooltip>}>
+              <button
+                className="btn btn-sm border border-0"
+                style={{ fontSize: '24px' }}
+                onClick={() => exportarUsuariosCSV()}>
+                <i className="bi bi-filetype-csv"></i>
+              </button>
+            </OverlayTrigger>
+          </div>
+          <IfContainer show={rolEnEmpleadorActual === 'administrador'}>
             <GuiaUsuario
               guia={listaguia[1]!?.activo && guia}
               placement="top-start"
@@ -188,8 +227,8 @@ const UsuariosPage: React.FC<UsuariosPageProps> = ({}) => {
               ref={agregaUsuario}>
               + Agregar Usuario
             </button>
-          </Col>
-        </IfContainer>
+          </IfContainer>
+        </Col>
       </Row>
 
       <div className="row mt-3">
