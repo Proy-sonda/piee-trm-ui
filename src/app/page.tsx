813510ec028e4cd00c94e9e1e208561cd38d6999
aruizaguila <alonso.ruiz@sonda.com>
2024-12-11 12:08:50 -0300

    Usar imagenes en lugar de botones en pagina de desambiguacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-'use client';
-
 import { adsUrl } from '@/servicios';
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface HomePageProps {
@@ -19,16 +18,39 @@ export default function HomePage({ searchParams }: HomePageProps) {
             Médicas
           </h1>
           <p>
-            Si es la primera vez que ingresa al portal presione el botón{' '}
+            Si es la primera vez que ingresa al portal presione la imagen{' '}
             <b>Inscribir Entidad Empleadora</b> para registrar su entidad empleadora y empezar a
-            usar el sistema. Si ya tiene inscrita su entidad presione el botón{' '}
+            usar el sistema. Si ya tiene inscrita su entidad empleadora, presione la imagen{' '}
             <b>Ingresar al Sistema</b> para tramitar sus licencias.
           </p>
         </div>
 
-        <div className="mt-4 d-flex align-items-center">
-          <div className="w-100 d-flex align-items-center justify-content-center">
+        <div className="mt-5 d-flex align-items-center">
+          <div className="w-100 d-flex flex-column flex-sm-row align-items-center justify-content-center">
             <Link
+              href={`${adsUrl()}/inicio-adscripcion`}
+              className="me-0 me-sm-5 text-decoration-none text-reset text-center">
+              <div
+                className="mx-auto position-relative"
+                style={{ width: '190px', height: '190px' }}>
+                <Image fill alt="Imagen de adscripcion" src="/adscripcion.png" />
+              </div>
+              <p className="text-center mt-2 mt-sm-3 mb-0">Inscribir Entidad Empleadora</p>
+            </Link>
+
+            <Link
+              href="/login"
+              className="ms-0 mt-5 mt-sm-0 ms-sm-5 text-decoration-none text-reset">
+              <div
+                className="mx-auto position-relative"
+                style={{ width: '190px', height: '190px' }}>
+                <Image fill alt="Imagen de adscripcion" src="/logintramitacion.png" />
+              </div>
+              <p className="text-center mt-2 mt-sm-3 mb-0">Ingresar al sistema</p>
+            </Link>
+
+            {/* VERSION CON BOTONES */}
+            {/* <Link
               href={`${adsUrl()}/inicio-adscripcion`}
               className="fs-6 text-center btn btn-primary btn-lg"
               style={{ width: '200px', height: '70px', alignContent: 'center' }}>
@@ -40,9 +62,10 @@ export default function HomePage({ searchParams }: HomePageProps) {
               className="fs-6 text-center btn btn-primary btn-lg ms-5"
               style={{ width: '200px', height: '70px', alignContent: 'center' }}>
               Ingresar al Sistema
-            </Link>
+            </Link> */}
           </div>
 
+          {/* VERSION ORIGINAL */}
           {/* <IfContainer show={searchParams.redirectTo}>
             <div className="col-12">
               <div
